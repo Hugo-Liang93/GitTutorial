a1f2c9ba045b5555dda39a5cba53db83b9e58903
Hugonbosss <hugo-lwh@hotmail.com>
2019-04-18 17:23:30 +0800

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by 梁文辉 on 2019/4/18.
+//  Copyright © 2019 梁文辉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
